be05ee0c5916497a1d3d2a9c224883358c2b361a
Maresca5 <166833830+Maresca5@users.noreply.github.com>
2026-02-26 15:54:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1300,8 +1300,6 @@ if not historical.empty:
                                 format="%.2f%%",
                                 key="full_rank_slider"
                             )
- # Dodatkowe wyświetlanie w procentach
-        st.caption(f"Wybrano: {prog_filtr:.0%}")
                         with col_f3:
                             only_value_full = st.checkbox("Tylko EV>0", key="full_rank_value")
                         
